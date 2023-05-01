b3b231f2f644a1fb8788060765c0a82820c7bf8f
Yannick Boudjelal <ndjbouman@gmail.com>
2023-05-01 19:02:59 +0200

    Correction

--- frontend/src/services/Loader.jsx
+++ frontend/src/services/Loader.jsx
@@ -5,7 +5,7 @@ import Spinner from "../components/Spinner";
 function Loader({ foldername, filename }) {
   console.log(import.meta.env.VITE_PUBLIC_URL);
   const DynamicComponent = lazy(() =>
-    import(`./${import.meta.env.VITE_PUBLIC_URL}/${foldername}/${filename}.jsx`)
+    import(`./${import.meta.env.VITE_PUBLIC_URL}${foldername}/${filename}.jsx`)
   );
 
   return (
