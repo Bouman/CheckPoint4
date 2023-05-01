5594ac094bf1a8b7d2a4b7568b6565398a078ffa
Yannick Boudjelal <ndjbouman@gmail.com>
2023-05-01 18:57:14 +0200

    Log Lien loader

--- frontend/src/services/Loader.jsx
+++ frontend/src/services/Loader.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Spinner from "../components/Spinner";
 
 function Loader({ foldername, filename }) {
+  console.log(import.meta.env.VITE_PUBLIC_URL);
   const DynamicComponent = lazy(() =>
     import(`./${import.meta.env.VITE_PUBLIC_URL}/${foldername}/${filename}.jsx`)
   );
